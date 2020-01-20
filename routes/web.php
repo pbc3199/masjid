ff90a091a42ce34c8acede1aced3b7e2f251f24a
pbc3199 <faizal.is77@gmail.com>
2020-01-20 16:03:34 +0800

    add new edit,index for senarai penceramah. Add new route for senarai penceramah

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Senarai Ahli Kariah
 Route::get('/ahlikariah','AhliKariahController@index');
 Route::post('/ahlikariah/create','AhliKariahController@create');
 Route::get('/ahlikariah/{id}/edit','AhliKariahController@edit');
 Route::post('/ahlikariah/{id}/update','AhliKariahController@update');
 Route::get('/ahlikariah/{id}/delete','AhliKariahController@delete');
+
+//Senarai Penceramah
+Route::get('/senaraipenceramah','SenaraiPenceramahController@spall');
